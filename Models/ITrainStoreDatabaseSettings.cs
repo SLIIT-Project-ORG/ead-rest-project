9d16ca81c440fa388df27420892019b518114191
Tharuka Gayashan <tharuka.gayashan.5@gmail.com>
2023-10-05 11:25:51 +0530

    Train Management APIs

---
+++ Models/ITrainStoreDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace ead_rest_project.Models
+{
+    public interface ITrainStoreDatabaseSettings
+    {
+        string TrainCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
---
+++ services/ITrainService.cs
@@ -0,0 +1,14 @@
+﻿using ead_rest_project.Models;
+using MongoDB.Driver;
+
+namespace ead_rest_project.services
+{
+    public interface ITrainService
+    {
+        List<Train> GetAllTrains();
+        Optional<Train> GetTrainById(string id);
+        Train CreateTrain(Train train);
+        void UpdateTrain(string id, Train train);
+        void DeleteTrain(string id);
+    }
+}
