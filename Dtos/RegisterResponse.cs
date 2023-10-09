f4f2841628cb59ca95e56155a971c334c542e878
Sachini Rasanga <sachisach983@gmail.com>
2023-10-09 20:05:57 +0530

    user apiss

---
+++ Dtos/RegisterResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace ead_rest_project.Dtos
+{
+	public class RegisterResponse
+	{
+		public string Message { get; set; } = string.Empty;
+		public bool Success { get; set; }
+
+	}
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDbGenericRepository.Attributes;
+
+namespace ead_rest_project.Models
+{
+	[CollectionName("users")]
+	public class ApplicationUser : MongoIdentityUser<Guid>
+
+	{
+		public string FullName { get; set; } = String.Empty;
+
+	}
+}
