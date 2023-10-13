8b506d65f3ea8bf747c9b8be451427aa8ee492d6
Tharuka Gayashan <tharuka.gayashan.5@gmail.com>
2023-10-13 20:57:34 +0530

    Comment part 2

--- Models/TrainStoreDatabaseSettings.cs
+++ Models/TrainStoreDatabaseSettings.cs
@@ -1,9 +1,21 @@
-﻿namespace ead_rest_project.Models
+﻿/*
+ Developed       : Tharuka Gayashan (IT20186906)
+ Function        : Train Management
+ Component Type  : Connection class
+ Filename        : TrainStoreDatabaseSettings.cs
+ Usage           : To get the details of the database connection
+*/
+
+
+namespace ead_rest_project.Models
 {
     public class TrainStoreDatabaseSettings : ITrainStoreDatabaseSettings
     {
+        //For database collection name => train
         public string TrainCollectionName { get; set; } = string.Empty;
+        //For database connection string => mongodb+srv://
         public string ConnectionString { get; set; } = string.Empty;
+        //For database name => ead-db
         public string DatabaseName { get; set; } = string.Empty;
     }
 }
